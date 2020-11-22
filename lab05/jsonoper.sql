1d4324f9e0d7235fdba895716ff39825f3088154
temasarkisov <a.sarkisov.s@yandex.ru>
2020-11-22 13:15:15 +0300

    Upload lab05

---
+++ lab05/jsonoper.sql
@@ -0,0 +1,71 @@
+-- 1. Extracts data from "driver" table into JSON file "driver.json"
+COPY (
+	SELECT row_to_json(driver_data) 
+    FROM (
+        SELECT *
+        FROM driver
+    ) driver_data 
+) TO '/Users/temasarkisov/OwnProjects/BMSTU/BMSTU-Data-Bases/lab05/driver.json';
+
+
+-- 2. Creates table "driver_based_json" based on JSON file "driver.json" data
+CREATE TABLE driver_based_json(
+    driver_id INTEGER, 
+    driver_ref VARCHAR, 
+    driver_number INTEGER,
+    code VARCHAR(3),
+    forename VARCHAR,
+    surname VARCHAR,
+    dob DATE,
+    nationality VARCHAR, 
+    PRIMARY KEY (driver_id)
+    );
+
+CREATE TABLE driver_json_tmp ( 
+    driver_info jsonb 
+);
+
+COPY driver_json_tmp 
+FROM '/Users/temasarkisov/OwnProjects/BMSTU/BMSTU-Data-Bases/lab05/driver.json';
+
+INSERT INTO driver_based_json(driver_id, driver_ref, driver_number, 
+            code, forename, surname, dob, nationality)
+	SELECT (driver_info -> 'driver_id')::INTEGER AS driver_id,
+           (driver_info ->> 'driver_ref') AS driver_ref,
+           (driver_info ->> 'driver_number')::INTEGER AS driver_number,
+           (driver_info ->> 'code') AS code,
+           (driver_info ->> 'forename') AS forename,
+           (driver_info ->> 'surname') AS surname,
+           (driver_info ->> 'dob')::DATE AS dob,
+           (driver_info ->> 'nationality') AS nationality
+	FROM driver_json_tmp;
+
+--CREATE TYPE driver_info_t 
+--AS (driver_id INTEGER, 
+--    driver_ref VARCHAR, 
+--    driver_number INTEGER,
+--    code VARCHAR(3),
+--    forename VARCHAR,
+--    surname VARCHAR,
+--    dob DATE,
+--    nationality VARCHAR
+--);
+
+--SELECT jsonb_each(driver_info)
+--FROM driver_json_tmp;
+
+--INSERT INTO driver_based_json
+--    SELECT jsonb_populate_record(NULL::driver_info_t, driver_info)
+--    FROM driver_json_tmp;
+
+
+-- 3. 
+CREATE TABLE driver_passport (
+    passport_number INT NOT NULL, 
+    driver_name JSON NOT NULL
+);
+
+INSERT INTO driver_passport 
+VALUES (19365687, '{"forename": "Lewis", "surname": "Hamilton"}'),
+       (75892093, '{"forename": "Artem", "surname": "Sarkisov"}'),
+       (94738401, '{"forename": "Max", "surname": "Verstappen"}');
